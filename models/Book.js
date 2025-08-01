c85fa6cb9e71d6e42ce60bf4cb1deeab18ecb6ae
Vlad Cirlugea <vlad.cirlugea@opswat.com>
2025-08-01 14:56:51 +0300

    Add Mongo connection

--- models/Book.js
+++ models/Book.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  author: { type: String, required: true },
-  id: { type: Number, required: true, unique: true }
+  author: { type: String, required: true }
+}, {
+  timestamps: true
 });
 
 module.exports = mongoose.model('Book', bookSchema);
